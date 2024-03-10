af456c787d3fecf9f28233e2024ffafc55466c9b
Nathan Glover <nathan@glovers.id.au>
2024-03-10 17:23:13 +0800

    fix: Added example project

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
-variable "name" {
+variable "prefix" {
   type        = string
-  description = "example name variable"
+  description = "Prefix for resources"
+  validation {
+    condition     = length(var.prefix) <= 15 && can(regex("^[a-zA-Z0-9-]*$", var.prefix))
+    error_message = "Prefix must be alphanumeric and hyphens only, and less than 15 characters."
+  }
 }
