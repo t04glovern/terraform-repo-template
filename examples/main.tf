bf961f1e385c420bd7dbd490374a1bc49aee5d4b
Nathan Glover <nathan@glovers.id.au>
2024-04-16 13:57:33 +0000

    fix: Refactored repo

--- examples/main.tf
+++ examples/main.tf
@@ -1,16 +1,8 @@
 provider "aws" {
-  region = local.region
-}
-
-locals {
   region = "us-east-1"
-}
-
-resource "random_pet" "this" {
-  length = 2
 }
 
 module "example" {
   source = "../"
-  prefix = random_pet.this.id
+  prefix = "abcdefg"
 }
--- lambda_layers.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  # https://docs.powertools.aws.dev/lambda/python/latest/
-  powertools_layer_versions = {
-    "us-east-1" = "arn:aws:lambda:us-east-1:017000801446:layer:AWSLambdaPowertoolsPythonV2:67",
-    "us-west-2" = "arn:aws:lambda:us-west-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:67"
-  }
-}
